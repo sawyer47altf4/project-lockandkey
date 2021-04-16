3fedb47e032674f532a6a2c4566ea169af18dddd
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2021-04-15 22:14:00 -0500

    not done
    not done still need to find a way to print the encoded text and get it

---
+++ lock.py
@@ -0,0 +1,43 @@
+
+
+
+print("welcom to lockandkey ")
+
+
+eord = input("encode or decode :")
+
+
+
+if (eord == "encode"):
+    ennum = input("encode key :")
+    if (ennum == "2bc4"):
+        a = "z"
+        b = "y"
+        c = "x"
+        d = "w"
+        e = "v"
+        f = "u"
+        g = "t"
+        h = "s"
+        i = "r"
+        j = "q"
+        k = "p"
+        l = "o"
+        m = "n"
+        n = "m"
+        o = "l"
+        p = "k"
+        q = "j"
+        r = "i"
+        s = "h"
+        t = "g"
+        u = "f"
+        v = "e"
+        w = "d"
+        x = "c"
+        y = "b"
+        z = "a"
+
+        encoder = input("put text :")
+
+        print(encoder)
